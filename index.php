6f3a2b8b083aea3c1ab2625d2835e770330d5257
zorro103 <chowee2@gmail.com>
2020-03-23 17:22:45 +0100

    Test Claude 01

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <header>
+        <h1>BIENVENUE LES 5 FANTASTIQUES</h1>
+    </header>
+    <main>
+        <section>
+            <p>CECI EST UNE TEST</p>
+        </section>
+    </main>
+    <footer>
+        <p>Tous droits réservés 2020</p>
+    </footer>
+    
+</body>
+</html>
