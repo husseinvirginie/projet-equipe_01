79c5c7479dd2e9c4c5e1250d7a7d29ef5b592d75
zorro103 <chowee2@gmail.com>
2020-04-10 11:27:27 +0200

    Last Envoi

--- php/view/section-reservation.php
+++ php/view/section-reservation.php
@@ -81,29 +81,100 @@
         &&$nombrecarte!=""){
 
         
-  $requeteSQL=
-
-<<<code
-
-INSERT INTO reservation
-
-(destination,date,duree,ville,adulte,enfant,nom,nombrecarte)
-
-VALUES
-
-(:destination,:date,:duree,:ville,:adulte,:enfant,:nom,:nombrecarte)
-
-code;
+        $requeteSQL=
+        <<<code
+        INSERT INTO reservation
+        (destination,date,duree,ville,adulte,enfant,nom,nombrecarte)
+        VALUES
+        (:destination,:date,:duree,:ville,:adulte,:enfant,:nom,:nombrecarte)
+        code;
 
 
-require_once "php/model/envoyer-sql.php";
+        require_once "php/model/envoyer-sql.php";
 
 
-echo "MERCI  $requeteSQL";
-} 
+        echo "MERCI  $requeteSQL";
+        } 
 
         ?>
       </div>
 
-      </form>
+  </form>
 </section>
+
+
+<section class="recherche">
+  
+    <h3 class="titre-recherche">Rechercher une Destination</h3>
+
+    <form class="search" action="">
+    <input type="text" name="recherche" required placeholder="Entrer un mot">
+    <button type="submit">Lancer la recherche</button>
+    </form>
+
+    <div class="listeJSON"></div>
+    
+
+    <script>
+    // FORMULAIRE DE RECHERCHE
+    // BLOQUER LE FORMULAIRE CLASSIQUE
+    var baliseForm  = document.querySelector(".search");
+    var baliseInput = document.querySelector("input[name=recherche]");
+    var listeJSON   = document.querySelector(".listeJSON");
+    var tableauJSON = [];   // AU DEPART LE TABLEAU EST VIDE
+
+    baliseForm.addEventListener("submit", function(event){
+      // POUR BLOQUER L'ENVOI DU FORMULAIRE
+      event.preventDefault();
+
+      // ON DOIT RECUPERER LE TEXTE SAISI
+      var texteSaisi = baliseInput.value;
+
+      // DEBUG
+      console.log(texteSaisi);
+
+      // ET ON DOIT ENVOYER UNE REQUETE AJAX VERS recherche-api.php
+      // ET ENSUITE ON RECUPERE LES INFOS JSON
+      // ET ON CONSTRUIT LE HTML POUR AFFICHER LES ARTICLES
+
+      // formData PERMET DE RAJOUTER DES INFORMATIONS SUPPLEMENTAIRES A ENVOYER VERS PHP
+      formData = new FormData();
+      // ON VA AJOUTER LE TEXTE SAISI
+      formData.append("recherche", texteSaisi);
+
+      fetch('recherche.php',
+      {
+          method: 'POST',
+          body:   formData
+      })
+      .then(function(reponseServeur) {
+          return reponseServeur.json();
+      })
+      .then(function(objetJSON) {
+          tableauJSON = objetJSON;
+
+      // ON VA REINITIALISER LA LISTE
+      listeJSON.innerHTML = "";
+      
+      for(var a=0; a < tableauJSON.length; a++)
+      {
+          listeJSON.innerHTML += 
+          `
+              <article>
+                  <h3>${tableauJSON[a].lieu}</h3>
+                  <h4>${tableauJSON[a].pays}</h4>
+                  <h5>${tableauJSON[a].categorie}</h5>
+                  <h6>${tableauJSON[a].saison}</h6>
+                  <p>${tableauJSON[a].description}</p>
+                
+                  <div class="prix">${tableauJSON[a].prix} €</div>
+  
+              </article>
+          `
+      }
+
+      });
+
+      });
+    </script>
+<section>
